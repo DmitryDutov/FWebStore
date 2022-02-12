cf469c677fe19e20c4a1e1fc45973486a9deb621
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-12 23:39:15 +0300

    Оставляем стандартный способ ASP NET Core 6

--- FWebStore/Program.cs
+++ FWebStore/Program.cs
@@ -1,23 +1,5 @@
-////var builder = WebApplication.CreateBuilder(args);
-////var app = builder.Build();
-
-//var app =  WebApplication.CreateBuilder(args).Build();
-//app.MapGet("/", () => "Hello World!");
-//app.Run();
-
-using System;
-
-namespace FWebStore
-{
-    internal partial class Program
-    {
-        public static void Main(string[] args)
-        {
-            var app =  WebApplication.CreateBuilder(args).Build();
-            app.MapGet("/", () => "Hello World!");
-            app.Run();
-
-        }
-    }
-}
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+app.Run();
 
