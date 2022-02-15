9748e748f2879479971254c415af91448944a3eb
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-16 00:29:24 +0300

    Регионы для пояснений

--- FWebStore/Program.cs
+++ FWebStore/Program.cs
@@ -1,14 +1,19 @@
 
-#region Части приложения
+#region Настройка построителя приложения - определение содержимого
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 services.AddControllersWithViews(); //Основная инфраструктура MVC
+
+#endregion
+
+#region Сборка приложения
+
 var app = builder.Build();
 
 #endregion
 
-#region Поведение приложения
+#region Конфигурирование объекта обработки входящих соединений
 
 //Поведение приложения в режиме Development
 if (app.Environment.IsDevelopment())
@@ -34,6 +39,9 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}" //Установили значения по умолчанию
     ); //кастомный маршрут
 
+#endregion
+
+#region Запуск приложения
 
 //Запуск приложения
 app.Run();
