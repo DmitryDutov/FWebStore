8e491648257232f19e12a7d81637a19cfa557c17
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-15 19:11:49 +0300

    Модель сотрудника

---
+++ FWebStore/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWebStore.Models;
+public class Employee
+{
+    public int Id { get; set; }             //Идентификатор
+    public string LastName { get; set; }    //Фамилия
+    public string FirstName { get; set; }   //Имя
+    public string Patronumic { get; set; }  //Отчество
+    public int Age { get; set; }            //Возраст
+}
