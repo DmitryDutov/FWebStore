9c199915edee919d58646146864187afe82f7ace
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-17 22:48:44 +0300

    Refa

--- FWebStore/Controllers/EmployeesController.cs
+++ FWebStore/Controllers/EmployeesController.cs
@@ -7,9 +7,9 @@ namespace FWebStore.Controllers
     {
         public static List<Employee> __Employees = new List<Employee>()
         {
-            new Employee{Id = 1, LastName = "L1", FirstName = "F1", Age = 34},
-            new Employee{Id = 2, LastName = "L2", FirstName = "F2", Age = 35},
-            new Employee{Id = 3, LastName = "L3", FirstName = "F2", Age = 36},
+            new Employee{Id = 1, LastName = "L1", FirstName = "F1",Patronumic = "P1", Age = 34},
+            new Employee{Id = 2, LastName = "L2", FirstName = "F2",Patronumic = "P2", Age = 35},
+            new Employee{Id = 3, LastName = "L3", FirstName = "F2",Patronumic = "P3", Age = 36},
         };
 
         public IActionResult Index()
