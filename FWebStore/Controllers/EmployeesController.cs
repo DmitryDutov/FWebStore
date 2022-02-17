7609e4721a1e341bf9cd79090fb47096a1d24218
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-17 13:35:23 +0300

    Создали контроллер Empliyees

---
+++ FWebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using FWebStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FWebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public static List<Employee> __Employees = new List<Employee>()
+        {
+            new Employee{Id = 1, LastName = "L1", FirstName = "F1", Age = 34},
+            new Employee{Id = 2, LastName = "L2", FirstName = "F2", Age = 35},
+            new Employee{Id = 3, LastName = "L3", FirstName = "F2", Age = 36},
+        };
+
+        public IActionResult Index()
+        {
+            var result = __Employees;
+            return View(result);
+        }
+    }
+}
+
--- FWebStore/Controllers/HomeController.cs
+++ FWebStore/Controllers/HomeController.cs
@@ -1,16 +1,9 @@
-﻿using FWebStore.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace FWebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public static List<Employee> __Employees = new List<Employee>()
-        {
-            new Employee{Id = 1, LastName = "L1", FirstName = "F1", Age = 34},
-            new Employee{Id = 2, LastName = "L2", FirstName = "F2", Age = 35},
-            new Employee{Id = 3, LastName = "L3", FirstName = "F2", Age = 36},
-        };
         public IActionResult Index()
         {
             //return Content("Данные из первого кортроллера");
@@ -20,11 +13,6 @@ namespace FWebStore.Controllers
         public string ConfiguredAction(string id, string value, string nextVal)
         {
             return $"ConfiguredAction - {id}: {value} + {nextVal}";
-        }
-
-        public IActionResult Employees()
-        {
-            return View(__Employees);
         }
     }
 }
