1bbba80e6a5a7992d80ed22bde91a8c5e88ebb23
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-19 23:19:32 +0300

    Перенос сотрудников

--- FWebStore/Controllers/EmployeesController.cs
+++ FWebStore/Controllers/EmployeesController.cs
@@ -1,26 +1,26 @@
-﻿using FWebStore.Models;
+﻿using FWebStore.Data;
+using FWebStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FWebStore.Controllers
 {
-    [Route("Empl/{action=Index}/{Id?}")]
-    [Route("Staff/{action=Index}/{Id?}")]
+    //[Route("Empl/{action=Index}/{Id?}")]
+    //[Route("Staff/{action=Index}/{Id?}")]
     public class EmployeesController : Controller
     {
-        public static List<Employee> __Employees = new List<Employee>()
+        private readonly ICollection<Employee> __Employees;
+        public EmployeesController()
         {
-            new Employee{Id = 1, LastName = "L1", FirstName = "F1",Patronumic = "P1", Age = 34},
-            new Employee{Id = 2, LastName = "L2", FirstName = "F2",Patronumic = "P2", Age = 35},
-            new Employee{Id = 3, LastName = "L3", FirstName = "F2",Patronumic = "P3", Age = 36},
-        };
-
+            __Employees = TestData.Employees;
+        }
+        
         public IActionResult Index()
         {
             var result = __Employees;
             return View(result);
         }
 
-        [Route("~/employees/info-{Id}")]
+        //[Route("~/employees/info-{Id}")]
         public IActionResult Details(int Id)
         {
             var employee = __Employees.FirstOrDefault(x => x.Id == Id);
