5225ba9a95ef0651a34992db53b487c5f529a8b4
DmitryDutov <dmitry_dutov@inbox.ru>
2022-03-10 11:51:41 +0300

    Добавил интерфейс сервиса каталога товаров

---
+++ FWebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,12 @@
+﻿using FWebStore.Domain.Entities;
+
+namespace FWebStore.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
+
