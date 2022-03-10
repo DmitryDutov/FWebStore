9fd0d479bd07a4a69df67fbf0822f338c949ec2e
DmitryDutov <dmitry_dutov@inbox.ru>
2022-03-10 11:44:04 +0300

    Созданы сущности секции и бренда

---
+++ FWebStore.Domain/Entities/Brand.cs
@@ -0,0 +1,11 @@
+﻿using FWebStore.Domain.Entities.Base;
+using FWebStore.Domain.Entities.Base.Interfaces;
+
+namespace FWebStore.Domain.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
+
---
+++ FWebStore.Domain/Entities/Section.cs
@@ -0,0 +1,13 @@
+﻿using FWebStore.Domain.Entities.Base;
+using FWebStore.Domain.Entities.Base.Interfaces;
+
+namespace FWebStore.Domain.Entities
+{
+    public class Section : NamedEntity,IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParrentId { get; set; } //ссылка на родительскую секцию. ? -> может не быть родтельской секции
+    }
+}
+
