0865f79c8403c0ce1b29289611f3b53c1447fdb0
DmitryDutov <dmitry_dutov@inbox.ru>
2022-04-20 15:30:57 +0300

    Создание viewcomponent Brand

---
+++ FWebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FWebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
