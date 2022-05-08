6b7ff4c5d19687a3ccf00ebc99f5c3b0884808d7
DmitryDutov <dmitry_dutov@inbox.ru>
2022-05-08 16:19:49 +0300

    ViewModel для секции

--- FWebStore/Components/SectionsViewComponent.cs
+++ FWebStore/Components/SectionsViewComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FWebStore.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FWebStore.Components
@@ -10,7 +11,19 @@ namespace FWebStore.Components
     //[ViewComponent (Name = "sections")]
     public class SectionsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IProductData _ProductData;
+
+        public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+        public IViewComponentResult Invoke()
+        {
+            //Получаем все секции
+            var sections = _ProductData.GetSections(); 
+
+            //Теперь создаём иерархическую структуру этих секций (для удбного отображения будем получать структуру через ViewModel)
+
+
+            return View();
+        }
     }
 }
 
---
+++ FWebStore/ViewModels/SectionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWebStore.ViewModels
+{
+    public class SectionViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public SectionViewModel? Parent { get; set; }
+        public List<SectionViewModel> ChidSection { get; set; } = new();
+    }
+}
