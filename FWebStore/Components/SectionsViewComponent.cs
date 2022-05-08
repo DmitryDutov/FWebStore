46aa644382b029b3c993c622fa5a73878ad8cf4a
DmitryDutov <dmitry_dutov@inbox.ru>
2022-05-08 15:45:15 +0300

    Создал визуальные компоненты

---
+++ FWebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FWebStore.Components
+{
+    //[ViewComponent (Name = "sections")]
+    public class SectionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
+
