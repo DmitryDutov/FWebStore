8bdf4539bbe5f0a21517791cb6653a6848053975
DmitryDutov <dmitry_dutov@inbox.ru>
2022-05-08 19:15:04 +0300

    Визуальная часть компонента секции

--- FWebStore/Components/SectionsViewComponent.cs
+++ FWebStore/Components/SectionsViewComponent.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FWebStore.Services.Interfaces;
+﻿using FWebStore.Services.Interfaces;
 using FWebStore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
