fc39f5aa740e4a986b60a44475862706509452eb
DmitryDutov <dmitry_dutov@inbox.ru>
2022-05-08 19:31:44 +0300

    Компонент брендов (готов)

--- FWebStore/Components/BrandsViewComponent.cs
+++ FWebStore/Components/BrandsViewComponent.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FWebStore.Services.Interfaces;
+using FWebStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FWebStore.Components
 {
     public class BrandsViewComponent : ViewComponent
     {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            return View(GetBrands());
         }
+
+        private IEnumerable<BrandViewModel> GetBrands() =>
+            _ProductData.GetBrands()
+                .OrderBy(b => b.Order)
+                .Select(b => new BrandViewModel
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                });
     }
 }
 
---
+++ FWebStore/ViewModels/BrandViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FWebStore.ViewModels
+{
+    public class BrandViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
