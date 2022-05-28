0d226bd4e8d0cc2ad08ef805e187c4039af63a42
DmitryDutov <dmitry_dutov@inbox.ru>
2022-05-28 16:55:49 +0300

    Create New Employee.cs

---
+++ FWebStore.Domain/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using FWebStore.Domain.Entities.Base;
+using FWebStore.Domain.Entities.Base.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+//Для работы с индексами БД нужен пакет Microsoft.EntityFrameworkCore.Abstractions
+namespace FWebStore.Domain.Entities
+{
+    [Index(nameof(Id))]
+    public class Employee : Entity, IOrderedEntity
+    {
+        public new int Id { get; set; }             //Идентификатор
+        public string LastName { get; set; }    //Фамилия
+        public string FirstName { get; set; }   //Имя
+        public string Patronumic { get; set; }  //Отчество
+        public int Age { get; set; }            //Возраст
+        public int Order { get; set; }
+    }
+}
+
--- FWebStore/Data/TestData.cs
+++ FWebStore/Data/TestData.cs
@@ -5,11 +5,11 @@ namespace FWebStore.Data
 {
     public static class TestData
     {
-        public static List<Employee> Employees { get; } = new List<Employee>()
+        public static List<Models.Employee> Employees { get; } = new List<Models.Employee>()
         {
-            new Employee{Id = 1, LastName = "L1", FirstName = "F1",Patronumic = "P1", Age = 34},
-            new Employee{Id = 2, LastName = "L2", FirstName = "F2",Patronumic = "P2", Age = 35},
-            new Employee{Id = 3, LastName = "L3", FirstName = "F2",Patronumic = "P3", Age = 36},
+            new Models.Employee{Id = 1, LastName = "L1", FirstName = "F1",Patronumic = "P1", Age = 34},
+            new Models.Employee{Id = 2, LastName = "L2", FirstName = "F2",Patronumic = "P2", Age = 35},
+            new Models.Employee{Id = 3, LastName = "L3", FirstName = "F2",Patronumic = "P3", Age = 36},
         };
 
                 public static IEnumerable<Section> Sections { get; } = new[]
