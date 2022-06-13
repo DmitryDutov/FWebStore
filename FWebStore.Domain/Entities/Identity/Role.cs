f10321f4f4f9ac6971877f56c913f54f77a6a737
DmitryDutov <dmitry_dutov@inbox.ru>
2022-06-13 15:17:42 +0300

    Определили пользователя и роль

---
+++ FWebStore.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FWebStore.Domain.Entities.Identity;
+
+public class Role : IdentityRole
+{
+
+}
---
+++ FWebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FWebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
+
