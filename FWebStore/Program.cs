63193beb54aa2f5a6f13809c4211036e933baf96
DmitryDutov <dmitry_dutov@inbox.ru>
2022-06-13 16:05:23 +0300

    Конфигурируем систему Identity

--- FWebStore/Program.cs
+++ FWebStore/Program.cs
@@ -1,11 +1,12 @@
 
     using FWebStore.DAL.Context;
+    using FWebStore.Domain.Entities.Identity;
     using FWebStore.Infrastructure.Conventions;
     using FWebStore.Infrastructure.Middleware;
     using FWebStore.Services;
-    using FWebStore.Services.InMemory;
     using FWebStore.Services.InSQL;
     using FWebStore.Services.Interfaces;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
 
     var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +26,33 @@
     services.AddDbContext<FWebStoreDB>(opt => 
         opt.UseSqlServer(/*указывем строку подключения*/ builder.Configuration.GetConnectionString("SqlServer"))); //в данном случаем строка подключения указана в файле конфигурации
     services.AddTransient<IDbInitializer, DbInitializer>();
+
+    //Подключаем систему Identity
+    services.AddIdentity<User, Role>()
+        .AddEntityFrameworkStores<FWebStoreDB>()
+        .AddDefaultTokenProviders();
+
+    //Конфигурируем систему Identity
+    services.Configure<IdentityOptions>(opt =>
+    {
+    #if DEBUG //Требования к паролю будут выполняться только в режиме Debug
+        opt.Password.RequireDigit = false; //Убираем требование наличия цифры в пароле
+        opt.Password.RequireLowercase = false; //Убираем требования к нижнему регистру
+        opt.Password.RequireUppercase = false; //Убираем требования к вернему регистру
+        opt.Password.RequireNonAlphanumeric = false; //Отключаем не алфавитные символы
+        opt.Password.RequiredLength = 3; //Длинна пароля = 3 символа
+        opt.Password.RequiredUniqueChars = 3; //Должно быть миниммум три ункальных символа
+    #endif
+
+        opt.User.RequireUniqueEmail = false; //Отключаем ункальность Email'ов для пользователей, т.к. мы не будем ими пользоваться
+        opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //Указваем разрешённый набор символов для имени пользователя
+
+        opt.Lockout.AllowedForNewUsers = false; //Отключаем блокировку новых пользователей (чтобы полсле создания пользователя не требовалось подтверждение)
+        opt.Lockout.MaxFailedAccessAttempts = 10; //Максимальное количество попыток угадать свой пароль
+        opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); //Указываем время блокировки
+    });
+
+    //Конфигурируем систему Cookies
 
     //services.AddSingleton<IEmployeesData, InMemoryEmpoyeesData>(); //Singleton - потому что InMemory !!!
     services.AddScoped<IEmployeesData, SqlEmployeesData>();
